bf0de51da9a898b2e2db55b3c004c006048d1d07
kl-botsu <botsu@khonsulabs.com>
2022-12-16 14:57:42 +0000

    Deploying to gh-pages from @ khonsulabs/okaywal@3e84a36a35b381dc61cddd4fcd3415cc5d8edf4d 🚀

--- main/implementors/core/cmp/trait.PartialOrd.js
+++ main/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["okaywal"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>&gt; for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>","synthetic":false,"types":["okaywal::entry::EntryId"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"okaywal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>&gt; for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/implementors/std/io/trait.Read.js
+++ main/implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["okaywal"] = [{"text":"impl&lt;'chunk, 'entry&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"okaywal/struct.EntryChunk.html\" title=\"struct okaywal::EntryChunk\">EntryChunk</a>&lt;'chunk, 'entry&gt;","synthetic":false,"types":["okaywal::log_file::EntryChunk"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"okaywal":[["impl&lt;'chunk, 'entry&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"okaywal/struct.EntryChunk.html\" title=\"struct okaywal::EntryChunk\">EntryChunk</a>&lt;'chunk, 'entry&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"okaywal/struct.ChunkReader.html\" title=\"struct okaywal::ChunkReader\">ChunkReader</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
