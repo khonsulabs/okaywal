86e484e6883f2d99b0d41bfa05991c5beb230b90
kl-botsu <botsu@khonsulabs.com>
2023-02-08 04:36:27 +0000

    Deploying to gh-pages from @ khonsulabs/okaywal@03ec20a04c485a7b1dd39e60c5c60259e9008306 🚀

--- main/implementors/core/cmp/trait.PartialOrd.js
+++ main/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"okaywal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>&gt; for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>"]]
+"okaywal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>&gt; for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
