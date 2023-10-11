aa6456e3069b02813207286ce7a3f913d9403ceb
kl-botsu <botsu@khonsulabs.com>
2023-10-11 16:50:12 +0000

    Deploying to gh-pages from @ khonsulabs/okaywal@01d2145ef54a98ad85c9b5801e3954fa81cd26a2 🚀

--- main/implementors/core/hash/trait.Hash.js
+++ main/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"okaywal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>"]]
+"okaywal":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"okaywal/struct.EntryId.html\" title=\"struct okaywal::EntryId\">EntryId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/src-files.js
+++ main/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "okaywal":["",[],["buffered.rs","config.rs","entry.rs","lib.rs","log_file.rs","manager.rs","to_io_result.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
